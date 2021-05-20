68c41fd7241c94c07de961254863b69b06628515
Adi1110 <ranjan.aditya03@gmail.com>
2021-05-20 18:12:50 +0530

    20/05/2021

--- src/main/java/com/exam/controller/UserController.java
+++ src/main/java/com/exam/controller/UserController.java
@@ -13,6 +13,7 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin("*")
 public class UserController {
 
     @Autowired
@@ -22,6 +23,7 @@ public class UserController {
     @PostMapping("/")
     public User createUser(@RequestBody User user) throws Exception {
 
+        user.setProfile("default.png");
         Set<UserRole> roleSet = new HashSet<>();
 
         Role role = new Role();
